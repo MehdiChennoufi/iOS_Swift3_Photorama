d223ea737134b3048e970a83b86a0b47dd0a50c1
MehdiChennoufi <mchennou@gmail.com>
2017-06-06 14:16:43 +0200

    initial commit

---
+++ Photorama/Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Mehdi Chennoufi on 31/05/2017.
+//  Copyright © 2017 Mehdi Chennoufi. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
